a691eab71d171d3d52f7e7bc797ef135a954d0b4
JhayStark <jaamen5@gmail.com>
2024-07-08 12:45:15 +0000

    chore: image and background change

--- components/IdCard.jsx
+++ components/IdCard.jsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import React from "react";
-import QRCode from "react-qr-code";
+import Image from 'next/image';
+import React from 'react';
+import QRCode from 'react-qr-code';
 
 const IdCard = ({
   firstName,
@@ -14,56 +14,56 @@ const IdCard = ({
   return (
     <>
       <div
-        className="w-[550px] h-[300px] bg-[#00467A] px-[19px]   flex flex-row justify-between py-5  "
+        className='w-[550px] h-[300px] bg-white px-[19px]   flex flex-row justify-between py-5  '
         ref={reference}
       >
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-row items-center justify-between gap-[57px]">
-            <div className="flex flex-col gap-1">
-              <p className="font-bold text-white">First Name</p>
-              <p className="font-bold text-[#D37E25]">
-                {firstName || "Enter first name"}
+        <div className='flex flex-col gap-4'>
+          <div className='flex flex-row items-center justify-between gap-[57px]'>
+            <div className='flex flex-col gap-1'>
+              <p className='font-bold text-black'>First Name</p>
+              <p className='font-bold text-[#D37E25]'>
+                {firstName || 'Enter first name'}
               </p>
             </div>
-            <div className="flex flex-col gap-1">
-              <p className="font-bold text-white ">Last Name</p>
-              <p className="font-bold text-[#D37E25]">
-                {lastName || "Enter last name"}
+            <div className='flex flex-col gap-1'>
+              <p className='font-bold text-black '>Last Name</p>
+              <p className='font-bold text-[#D37E25]'>
+                {lastName || 'Enter last name'}
               </p>
             </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <p className="font-bold text-white ">Location</p>
-            <p className="font-bold text-[#D37E25]">
-              {location || "Enter location"}
+          <div className='flex flex-col gap-1'>
+            <p className='font-bold text-black '>Location</p>
+            <p className='font-bold text-[#D37E25]'>
+              {location || 'Enter location'}
             </p>
           </div>
-          <div className="flex flex-col gap-1">
-            <p className="font-bold text-white ">Date of Issuance</p>
-            <p className="font-bold text-[#D37E25]">
-              {dateOfIssuance || "Enter Date of Issuance"}
+          <div className='flex flex-col gap-1'>
+            <p className='font-bold text-black '>Date of Issuance</p>
+            <p className='font-bold text-[#D37E25]'>
+              {dateOfIssuance || 'Enter Date of Issuance'}
             </p>
           </div>
-          <div className="flex flex-col gap-1">
-            <p className="font-bold text-white ">Date of Expiry</p>
-            <p className="font-bold text-[#D37E25]">
-              {dateOfExpiry || "Enter Date of Expiry"}
+          <div className='flex flex-col gap-1'>
+            <p className='font-bold text-black '>Date of Expiry</p>
+            <p className='font-bold text-[#D37E25]'>
+              {dateOfExpiry || 'Enter Date of Expiry'}
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-between h-full ">
-          <p className="font-bold text-white">
-            ID :{" "}
-            <span className="text-[#D37E25]">{idNumber || "Enter ID"}</span>
+        <div className='flex flex-col items-center justify-between h-full '>
+          <p className='font-bold text-black'>
+            ID :{' '}
+            <span className='text-[#D37E25]'>{idNumber || 'Enter ID'}</span>
           </p>
           <QRCode
-            title="GeeksForGeeks"
+            title='GeeksForGeeks'
             value={idNumber}
-            bgColor="#00467A"
-            fgColor="white"
+            bgColor='#00467A'
+            fgColor='white'
             size={120}
           />
-          <Image width={90} height={18} alt="logo" src="/esoko-logo.svg" />
+          <Image width={90} height={18} alt='logo' src='/aml-logo.png' />
         </div>
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import IdCard from "@/components/IdCard";
-import React, { useRef, useState } from "react";
-import * as htmlToImage from "html-to-image";
+import IdCard from '@/components/IdCard';
+import React, { useRef, useState } from 'react';
+import * as htmlToImage from 'html-to-image';
 
 const Index = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [idNumber, setIdNumber] = useState("");
-  const [location, setLocation] = useState("");
-  const [dateOfIssuance, setDateOfIssuance] = useState("");
-  const [dateOfExpiry, setDateOfExpiry] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [idNumber, setIdNumber] = useState('');
+  const [location, setLocation] = useState('');
+  const [dateOfIssuance, setDateOfIssuance] = useState('');
+  const [dateOfExpiry, setDateOfExpiry] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
   const idCardRef = useRef();
 
@@ -18,75 +18,75 @@ const Index = () => {
     htmlToImage
       .toSvg(idCardRef.current)
       .then(function (dataUrl) {
-        alert("Id Card Generated Successfully");
+        alert('Id Card Generated Successfully');
         setImageUrl(dataUrl);
       })
       .catch(function (error) {
-        alert("Error generating ID card");
-        console.error("Error generating ID card:", error);
+        alert('Error generating ID card');
+        console.error('Error generating ID card:', error);
       });
   };
 
   const handleDownload = () => {
-    const link = document.createElement("a");
+    const link = document.createElement('a');
     link.href = imageUrl;
     link.download = `${firstName + lastName}.svg`;
     link.click();
   };
 
   return (
-    <div className="flex flex-row bg-white shadow-md rounded-2xl">
-      <div className="flex flex-col gap-5 p-8 min-w-[30rem]">
-        <div className="flex flex-col justify-start gap-2">
-          <label htmlFor="firstName">First Name</label>
+    <div className='flex flex-row bg-white shadow-md rounded-2xl'>
+      <div className='flex flex-col gap-5 p-8 min-w-[30rem]'>
+        <div className='flex flex-col justify-start gap-2'>
+          <label htmlFor='firstName'>First Name</label>
           <input
-            type="text"
-            className="p-2 border-2 rounded-lg focus:outline-none"
-            onChange={(e) => setFirstName(e.target.value)}
+            type='text'
+            className='p-2 border-2 rounded-lg focus:outline-none'
+            onChange={e => setFirstName(e.target.value)}
           />
         </div>
-        <div className="flex flex-col justify-start gap-2">
-          <label htmlFor="firstName">Last Name</label>
+        <div className='flex flex-col justify-start gap-2'>
+          <label htmlFor='firstName'>Last Name</label>
           <input
-            type="text"
-            className="p-2 border-2 rounded-lg focus:outline-none"
-            onChange={(e) => setLastName(e.target.value)}
+            type='text'
+            className='p-2 border-2 rounded-lg focus:outline-none'
+            onChange={e => setLastName(e.target.value)}
           />
         </div>
-        <div className="flex flex-col justify-start gap-2">
-          <label htmlFor="firstName">ID Number</label>
+        <div className='flex flex-col justify-start gap-2'>
+          <label htmlFor='firstName'>ID Number</label>
           <input
-            type="text"
-            className="p-2 border-2 rounded-lg focus:outline-none"
-            onChange={(e) => setIdNumber(e.target.value)}
+            type='text'
+            className='p-2 border-2 rounded-lg focus:outline-none'
+            onChange={e => setIdNumber(e.target.value)}
           />
         </div>
-        <div className="flex flex-col justify-start gap-2">
-          <label htmlFor="firstName">Location</label>
+        <div className='flex flex-col justify-start gap-2'>
+          <label htmlFor='firstName'>Location</label>
           <input
-            type="text"
-            className="p-2 border-2 rounded-lg focus:outline-none"
-            onChange={(e) => setLocation(e.target.value)}
+            type='text'
+            className='p-2 border-2 rounded-lg focus:outline-none'
+            onChange={e => setLocation(e.target.value)}
           />
         </div>
-        <div className="flex flex-col justify-start gap-2">
-          <label htmlFor="firstName">Date of Issuance</label>
+        <div className='flex flex-col justify-start gap-2'>
+          <label htmlFor='firstName'>Date of Issuance</label>
           <input
-            type="text"
-            className="p-2 border-2 rounded-lg focus:outline-none"
-            onChange={(e) => setDateOfIssuance(e.target.value)}
+            type='text'
+            className='p-2 border-2 rounded-lg focus:outline-none'
+            onChange={e => setDateOfIssuance(e.target.value)}
           />
         </div>
-        <div className="flex flex-col justify-start gap-2">
-          <label htmlFor="firstName">Date of Expiry</label>
+        <div className='flex flex-col justify-start gap-2'>
+          <label htmlFor='firstName'>Date of Expiry</label>
           <input
-            type="text"
-            className="p-2 border-2 rounded-lg focus:outline-none"
-            onChange={(e) => setDateOfExpiry(e.target.value)}
+            type='text'
+            className='p-2 border-2 rounded-lg focus:outline-none'
+            onChange={e => setDateOfExpiry(e.target.value)}
           />
         </div>
       </div>
-      <div className="flex flex-col p-8 border-l-2 min-w-[400px] justify-between ">
+      <div className='flex flex-col p-8 border-l-2 min-w-[400px] justify-between '>
         <IdCard
           firstName={firstName}
           lastName={lastName}
@@ -96,16 +96,16 @@ const Index = () => {
           dateOfExpiry={dateOfExpiry}
           reference={idCardRef}
         />
-        <div className="flex self-end gap-5">
+        <div className='flex self-end gap-5'>
           <button
             onClick={handleGenerate}
-            className="p-2 text-white bg-blue-500 rounded-lg hover:opacity-80"
+            className='p-2 text-white bg-blue-500 rounded-lg hover:opacity-80'
           >
             Generate New ID Card
           </button>
           <button
             onClick={handleDownload}
-            className="p-2 text-white bg-green-500 rounded-lg hover:opacity-80"
+            className='p-2 text-white bg-green-500 rounded-lg hover:opacity-80'
           >
             Download ID Card
           </button>
